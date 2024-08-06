ba67f8b65c8ef4a9abee385223f0e0b001d91811
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-08-07 00:51:46 +0530

    Add files via upload

---
+++ Variables and Data Types/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package Variables_and_DataTypes;
+
+
+//8.Calculate the area of a circle given its radius
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        double radius = 4.5; //input
+
+        double rSquare = radius * radius;
+        double pi = 3.14;
+
+        System.out.println(rSquare * pi);
+
+    }
+}
