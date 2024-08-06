13d2fce9242101adb4277b2f6f16cc2fd65bbe44
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-08-07 00:42:09 +0530

    Add files via upload

---
+++ Variables and Data Types/TypeCast.java
@@ -0,0 +1,13 @@
+package Variables_and_DataTypes;
+
+//7.Demonstrate type casting in Java
+
+public class TypeCast {
+    public static void main(String[] args) {
+
+        double myDouble = 9.78;
+        System.out.println("Before casting :" + myDouble);
+        System.out.println("After casting:" + (int) myDouble);
+
+    }
+}
