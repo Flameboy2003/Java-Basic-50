dd8daabfca1da0b420206770701b1d9e55da5e88
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-08-07 00:36:54 +0530

    Add files via upload

---
+++ Variables and Data Types/Ascii.java
@@ -0,0 +1,12 @@
+package Variables_and_DataTypes;
+
+
+//6.Find the ASCII value of a character
+
+public class Ascii {
+    public static void main(String[] args) {
+        char ch=97;
+        System.out.println(ch);
+    }
+
+}
