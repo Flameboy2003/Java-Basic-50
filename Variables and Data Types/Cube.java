d2e1d1783efc7b0d596a6955e572e847abe8ce0f
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-08-07 00:54:10 +0530

    Add files via upload

---
+++ Variables and Data Types/Cube.java
@@ -0,0 +1,14 @@
+package Variables_and_DataTypes;
+
+public class Cube {
+    public static void main(String[] args) {
+        // Initialize the number to be cubed
+        int num = 3;
+
+        // Calculate the cube of the number
+        int cube = num * num * num;
+
+        // Print the result
+        System.out.println("The cube of " + num + " is " + cube);
+    }
+}
