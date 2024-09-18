0e66c2493033d6573a5d44446463d9ff43712876
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2024-09-18 22:42:32 +0530

    Update Addition.java

--- Operators/Addition.java
+++ Operators/Addition.java
@@ -2,7 +2,7 @@ package Operator;
 
 import java.util.Scanner;
 
-public class Addition {
+public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
